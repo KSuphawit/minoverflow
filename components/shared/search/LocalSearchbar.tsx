ae87d3f90254121aeaf7ba8077c1c9fbfe5bb04d
suphawit-k <suphawit.k@linecorp.com>
2023-11-21 16:44:46 +0700

    impl: local searchbar

---
+++ components/shared/search/LocalSearchbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+
+interface CustomInputProps {
+  route: string;
+  iconPosition: "left" | "right";
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+function LocalSearchbar({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: CustomInputProps) {
+  return (
+    <div className="relative w-full">
+      <div
+        className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-[10px] px-4 ${otherClasses}`}
+      >
+        {iconPosition === "left" && (
+          <Image
+            src={imgSrc}
+            alt="search icon"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+        <Input
+          type="text"
+          placeholder={placeholder}
+          className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+        />
+        {iconPosition === "right" && (
+          <Image
+            src={imgSrc}
+            alt="search icon"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LocalSearchbar;
