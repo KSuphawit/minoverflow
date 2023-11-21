2488dd4d2968abbe7e3ce03dd64264188de7a04e
suphawit-k <suphawit.k@linecorp.com>
2023-11-21 16:45:51 +0700

    fix: ui local searchbar

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -19,34 +19,32 @@ function LocalSearchbar({
   otherClasses,
 }: CustomInputProps) {
   return (
-    <div className="relative w-full">
-      <div
-        className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-[10px] px-4 ${otherClasses}`}
-      >
-        {iconPosition === "left" && (
-          <Image
-            src={imgSrc}
-            alt="search icon"
-            width={24}
-            height={24}
-            className="cursor-pointer"
-          />
-        )}
-        <Input
-          type="text"
-          placeholder={placeholder}
-          className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+    <div
+      className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          alt="search icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
         />
-        {iconPosition === "right" && (
-          <Image
-            src={imgSrc}
-            alt="search icon"
-            width={24}
-            height={24}
-            className="cursor-pointer"
-          />
-        )}
-      </div>
+      )}
+      <Input
+        type="text"
+        placeholder={placeholder}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+      />
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          alt="search icon"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
     </div>
   );
 }
